9619961bbc846090309c3636db843ed1d75d1f7c
khat <khaquachtrong74@gmail.com>
2025-09-22 10:41:00 +0700

    Move outside

--- url_module/address_str.js
+++
@@ -1,7 +0,0 @@
-var url = require("url")
-
-var curURL = url.parse(
-    'https://www.google.com/search?query=steve+jobs');
-var curStr = url.format(curURL);
-console.log('Address string: %s', curStr);
-console.dir(curURL);
